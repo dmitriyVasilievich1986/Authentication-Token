c06e86b329422f8b7784125530272b159b4533a5
dmitriy shcherbachenya <dmitriyvasil@gmail.com>
2021-01-05 15:29:19 +0300

    nginx

--- authentication/authentication/settings.py
+++ authentication/authentication/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "kgr94dxvg%g%5du%&e65h4a-7^)wu@+ep7z0wr+xpar1gx3r83"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
--- authentication/authentication/urls.py
+++ authentication/authentication/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 from knox.views import LoginView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/auth/", include("account.urls")),
+    path("", include("account.urls")),
 ]
